896651f127bbcaea886cb43faadc04e2156dcb4d
miguel <mavl@MacBook-Pro-de-Miguel.local>
2018-02-12 17:34:03 +0100

    Add breadcrumbs with observable and working with Metadata

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -10,18 +11,39 @@ import 'rxjs/add/operator/map';
 })
 export class BreadcrumbsComponent implements OnInit {
 
+  label: string = '';
+
   constructor(
-    private _router: Router
+    private _router: Router,
+    public _title: Title,
+    public _meta: Meta
   ) {
 
-    this._router.events
-        .filter( event => event instanceof ActivationEnd )
-        .filter( (event: ActivationEnd) => event.snapshot.firstChild === null )
-        .map( (event: ActivationEnd) => event.snapshot.data )
-        .subscribe( event => console.log(event) );
+      this.getDataRoute()
+          .subscribe( data => {
+            this.label = data.title;
+            this._title.setTitle(this.label);
+
+            const metaTag: MetaDefinition = {
+              name: 'description',
+              content: this.label
+            };
+
+            this._meta.updateTag(metaTag);
+
+          });
   }
 
   ngOnInit() {
+  }
+
+  getDataRoute () {
+
+    return this._router.events
+          .filter( event => event instanceof ActivationEnd )
+          .filter( (event: ActivationEnd) => event.snapshot.firstChild === null )
+          .map( (event: ActivationEnd) => event.snapshot.data );
+
   }
 
 }
