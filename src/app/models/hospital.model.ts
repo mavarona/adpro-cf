02d3da8401a06b47139962ed619912aa3eb20f2d
miguel <mavl@MacBook-Pro-de-Miguel.local>
2018-02-25 22:22:54 +0100

    create load hospitals in the grid

--- src/app/models/hospital.model.ts
+++ src/app/models/hospital.model.ts
@@ -1,7 +1,7 @@
 export class Hospital {
 
   constructor (
-      public nombre: string,
+      public name: string,
       public img?: string,
       public _id?: string
   ) { }
--- src/app/pages/hospitals/hospitals.component.ts
+++ src/app/pages/hospitals/hospitals.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+// Model
+import { Hospital } from '../../models/hospital.model';
+
+// Services
+import { HospitalService } from '../../services/hospital/hospital.service';
+
 @Component({
   selector: 'app-hospitals',
   templateUrl: './hospitals.component.html',
@@ -7,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HospitalsComponent implements OnInit {
 
-  constructor() { }
+  hospitals: Hospital[] = [];
+
+  constructor(
+    public _hospitalService: HospitalService
+  ) { }
 
   ngOnInit() {
+
+    this.loadHospitals();
+
+  }
+
+  loadHospitals () {
+
+
+    this._hospitalService.loadHospitals( )
+        .subscribe( hospitals => {
+
+          this.hospitals = hospitals;
+
+        });
+  }
+
+  searchHospitals ( term: string ) {
+
+
+
+  }
+
+  createHospital () {
+
+
+
+  }
+
+  saveHospital ( hospital: Hospital ) {
+
+
+  }
+
+  deleteHospital ( hospital: Hospital ) {
+
+
   }
 
 }
