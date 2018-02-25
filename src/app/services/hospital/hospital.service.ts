660f5a5258570e517ed816a0c97ec4038f9fd3ff
miguel <mavl@MacBook-Pro-de-Miguel.local>
2018-02-25 21:55:48 +0100

    service hospital create, delete, update...

--- src/app/services/hospital/hospital.service.ts
+++ src/app/services/hospital/hospital.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { URL_SERVICES } from '../../config/config';
 
+// Models
+import { Hospital } from '../../models/hospital.model';
+
+// Services
+import { UserService } from '../user/user.service';
+
 @Injectable()
 export class HospitalService {
 
   totalHospitals: number = 0;
 
   constructor(
-    public _http: HttpClient
+    public _http: HttpClient,
+    public _userService: UserService
   ) { }
 
   loadHospitals () {
@@ -30,6 +37,49 @@ export class HospitalService {
     return this._http.get( url )
                .map( (resp: any) => resp.hospital );
 
+  }
+
+  deleteHopspital ( id: string ) {
+
+    let url = URL_SERVICES + '/hospital/' + id;
+
+    url += '?token=' + this._userService.token;
+
+    return this._http.delete( url )
+               .map( resp => swal('Hospital Deleted', 'The hospital was deleted', 'success') );
+
+  }
+
+  createHospital ( name: string ) {
+
+    let url = URL_SERVICES + '/hospital';
+
+    url += '?token=' + this._userService.token;
+
+    return this._http.post( url, {name: name} )
+               .map( (resp: any) => resp.hospital );
+
+  }
+
+  searchHospital ( term: string ) {
+
+    const url = URL_SERVICES + '/search/collection/hospitals/' + term;
+    return this._http.get( url )
+               .map( (resp: any) => resp.hospitals );
+
+  }
+
+  updateHospital ( hospital: Hospital ) {
+
+    let url = URL_SERVICES + '/hospital/' + hospital._id;
+    url += '?token=' + this._userService.token;
+
+    return this._http.put( url, hospital )
+               .map( (resp: any) => {
+
+                return resp.hospital;
+
+               });
   }
 
 }
